4167a7b12ff9512d9e078a01fc17ebf72c1bc052
Ryan Wire <ryanwire@outlook.com>
2018-12-10 15:03:04 +0300

    [#162539149] Adds modifications to tests

--- forum_app/tests_comment_app.py
+++ forum_app/tests_comment_app.py
@@ -1,31 +1,37 @@
 import unittest
-import user, comment, moderator, admin
+from user import User
+from comment import Comment
+from admin import Admin
+from moderator import Moderator
 
 class UserTestCase(unittest.TestCase):
   def setUp(self):
-    user1 = User('JohnPaul')
-    mod1 = Moderator('Elisha')
-    admin1 = Admin('Anthonny')
-    comment = "Hello"
-    new_comment = "Hello World"
+    self.user1 = User('JohnPaul')
+    self.mod1 = Moderator('Elisha')
+    self.admin1 = Admin('Anthonny')
+    self.comment = "Hello"
+    self.new_comment = "Hello World"
+    self.edit_comment = User.edit_comment
+    self.delete_comment = User.delete_comment
+    self.log_in = User.log_in
 
   def test_instantiation(self):
-    self.assertIsInstance(mod1, user1, 'Moderator is a user')
-    self.assertIsInstance(admin1, user1, 'Admin is a user')
+    self.assertIsInstance(self.mod1, self.user1, 'Moderator is a user')
+    self.assertIsInstance(self.admin1, self.user1, 'Admin is a user')
 
   def test_username_is_set(self):
-    self.assertEqual(user1.name,'JohnPaul', 'User name is set')
-    self.assertEqual(mod1.name,'Elisha', 'User name is set')
-    self.assertEqual(admin1.name,'Anthonny', 'User name is set')
+    self.assertEqual(self.user1.username,'JohnPaul', 'User name is set')
+    self.assertEqual(self.mod1.username,'Elisha', 'User name is set')
+    self.assertEqual(self.admin1.username,'Anthonny', 'User name is set')
 
   def test_admin_can_edit_comment(self):
-    self.assertTrue(edit_comment(comment, new_comment))
+    self.assertTrue(self.edit_comment(self, self.comment, self.new_comment))
   
   def test_admin_can_delete_comment(self):
-    self.assertTrue(delete_comment(comment))
+    self.assertTrue(self.delete_comment(self, self.comment))
 
   def test_moderator_can_delete_comment(self):
-    self.assertTrue(delete_comment(comment))
+    self.assertTrue(self, self.delete_comment(self, self.comment))
 
   def test_user_can_login(self):
-    self.assertTrue(log_in("username", "password"))
+    self.assertTrue(self.log_in(self, "username", "password"))
