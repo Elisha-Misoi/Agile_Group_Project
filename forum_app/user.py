333b5e8f6fabac92f9cefb7602212d7815b4b739
Omulosi <mulongojohnpaul@gmail.com>
2018-12-10 13:19:18 +0300

    [Feature #162539126] Create login and logout methods

---
+++ forum_app/user.py
@@ -0,0 +1,34 @@
+
+from datetime import datetime
+# from comment import Comment
+
+class User(object):
+
+    def __init_(self, username, password):
+        self.username = username
+        self.password = password
+        self.loggedInAt = None
+        self.isLoggedIn = False
+        self.comment = None
+        self.isAdmin = False
+        self.isModerator = False
+
+    def log_in(self, username, password):
+        """
+        Takes a username and password.
+        Sets the isLoggedIn variable to True.
+        Sets the current logged in time
+        """
+
+        if username == self.username and password == self.password:
+            self.isLoggedIn = True
+            self.loggedInAt = datetime.utcnow()
+        return False
+
+    def log_out(self):
+        if self.isLoggedIn:
+            self.isLoggedIn = False
+            return True
+        return False
+
+
